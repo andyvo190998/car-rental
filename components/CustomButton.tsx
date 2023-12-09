b7b0ae1e0c5c49edd9a7d0bf76335c2e47927edf
Andy Vo <andyvo@Andys-MacBook-Pro.local>
2023-12-09 10:32:02 +0100

    desgin navbar

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,15 +1,17 @@
 'use client';
+import { CustomButtonProps } from '@/types';
 import React from 'react';
 
 const CustomButton = ({
   title,
   containerStyles,
   handleClick,
+  btnType,
 }: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={'button'}
+      type={btnType || 'button'}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
