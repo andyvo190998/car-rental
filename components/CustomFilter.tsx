f88f3ceac747f8b3efda05e424758458beaee004
Andy Vo <andyvo@Andys-MacBook-Pro.local>
2023-12-09 22:45:32 +0100

    filter list of brand

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,8 @@
+import { CustomButtonProps } from '@/types';
+import React from 'react';
+
+const CustomFilter = ({ title }: CustomButtonProps) => {
+  return <div>CustomFilter</div>;
+};
+
+export default CustomFilter;
