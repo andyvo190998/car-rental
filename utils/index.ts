3711c6eda58854f6809d41bbf5a9e078d2e8e313
Andy Vo <andyvo190998@gmail.com>
2023-12-24 13:10:36 +0100

    feat: add env

--- utils/index.ts
+++ utils/index.ts
@@ -7,8 +7,10 @@ export async function fetchCars(filters: FIlterProps) {
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
   };
 
+  const carApi = process.env.DB_CAR_API;
+
   const response = await fetch(
-    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${model}&limit=${limit}&fuel_type=${fuel}`,
+    `${carApi}?make=${manufacturer}&year=${model}&limit=${limit}&fuel_type=${fuel}`,
     {
       headers: headers,
     }
